096e7f5573422ca6e9b14cd7c7fce80284e21632
Suraj Malviya <suraj@Surajs-MacBook-Pro.local>
2022-04-07 00:44:42 +0530

    ft: add repository, apiservice, viewmodel elements with live data for UI to complete mvvm

--- app/src/main/java/com/example/mysweetmovieapp/repository/DataSourceUpdate.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mysweetmovieapp.repository
-
-import com.example.mysweetmovieapp.model.Movie
-
-interface DataSourceUpdate {
-    fun remoteMovieListUpdated(remoteMovies: List<Movie>)
-}
